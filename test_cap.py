611f919fe26f176d2ac4ee9ce694f69e2a352b2c
dhamu2github <dhamu2win@gmail.com>
2024-06-16 16:55:22 -0700

    all files are related python unit testing

---
+++ test_cap.py
@@ -0,0 +1,19 @@
+import unittest
+import cap
+
+class TestCap(unittest.TestCase):
+
+    def test_one_word(self):
+        text = 'python1'
+        result = cap.cap_text(text)
+        self.assertEqual(result,"Python")
+
+    def test_multiple_words(self):
+        text = "hello world"
+        result = cap.cap_text(text)
+        self.assertEqual(result,"Hello World")
+
+if __name__=='__main__':
+    unittest.main()
+
+
---
+++ without_error.py
@@ -0,0 +1,19 @@
+'''
+A simple program to learn Python Unit Testing
+Concepts:
+
+'''
+
+def myfunc():
+    '''
+    Create some variable and print it.
+
+    '''
+
+    varaible_1 = 10
+    varaiable_2 = 20
+
+    print(varaible_1)
+    print(varaiable_2)
+
+myfunc()
