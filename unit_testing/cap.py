bf200989249605b649ee4b4f6c345f01f5ec2f53
dhamu2github <dhamu2win@gmail.com>
2024-06-19 07:38:13 -0700

    moved the files to folder

---
+++ unit_testing/cap.py
@@ -0,0 +1,8 @@
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalized the first letter of the given string
+
+    Note: To fix the issue we change from text.capitalize() to text.title()
+    '''
+    return text.title()
