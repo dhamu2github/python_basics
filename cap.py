b1a283270e81909f5b5c6768466e61e863aaa8ba
dhamu2github <dhamu2win@gmail.com>
2024-06-19 07:40:32 -0700

    removed files

--- cap.py
+++
@@ -1,8 +0,0 @@
-def cap_text(text):
-    '''
-    Input a string
-    Output the capitalized the first letter of the given string
-
-    Note: To fix the issue we change from text.capitalize() to text.title()
-    '''
-    return text.title()
